971af461b78899f59eda7c859e4da5acb2bfe9b2
Jim Weist <jp_weist@yahoo.com>
2019-08-17 13:59:54 -0600

    I was able to get css for computer screen add JS for changing some dogs but could not get past that.

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
 var inputBox = document.querySelector('input');
 var nameDogButton = document.querySelector('button');
 
+
 nameDogButton.addEventListener('click', function() {
     var changeH1Dog = document.querySelector('h1 span');
     changeH1Dog.innerText = inputBox.value;
     console.log(inputBox.value);
 })
 
-
-
-
-// var buttonClick = document.querySelector('button');
+// var howToDogButton = document.querySelector('#howButton');
 //
-// buttonClick.addEventListener('click', function() {
-//   var inputBox = document.querySelector('input').value;
-//   var greeting = document.querySelector('p');
-//   return greeting.innerText = `Hi ${inputBox}, your awesome!`;
-// });
+// howToDogButton.addEventListener('click', function() {
+//   var howToDogText = document.querySelector('#howToDogId');
+//   howToDogText.classList.add('hidden');
+//
+// })
